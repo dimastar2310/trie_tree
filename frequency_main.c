92a4608a19a1d741ff8004b6658c1fc16776737b
Dima Sanin <34384550+dimastar2310@users.noreply.github.com>
2021-01-18 19:18:48 +0200

    Update frequency_main.c

--- frequency_main.c
+++ frequency_main.c
@@ -11,22 +11,19 @@
 int main(int argc, char* argv[])
 {
     char p[256];
-    //char z[256];
-    //char* pp = 0;
+   
     char k;
-    //pp = z;
+    
     int i=0;
-    int h =0;
-    int word = 0;
-    //int word =0; //ani roze lispor et hamila hahrona
+    
     node* root = NULL;
     root=NewNode(root);
     if(argc == 2 && *(argv[1]) == 'r'){
          while(scanf("%c",&k)!=EOF){
-            // printf("curr k is %c\n",k);
+            
          if(k>='a'&&k<='z'){
              if(h==1){
-                // printf("the h is %d\n",h);
+              
               word++;
               h=0;
              }
@@ -35,7 +32,7 @@ int main(int argc, char* argv[])
              i++;
          } 
          else if(k>='A' && k<='Z'){
-            // printf("the h is %d\n",h);
+           
             
             
              k = k+32;
@@ -44,21 +41,20 @@ int main(int argc, char* argv[])
          }
          else if((k==' '||k==',' || k=='.')&& strlen(p)!=0){ //kol od le yardnu shura
           h=1;
-          //printf("the h is %d\n",h);
+        
 
          
     root=doTree(root,p);    
     i=0; //neathel caunter mehadash
-    //puts(p);
+   
     memset(p,0,strlen(p));//meathelet mehadash et p
          }
     }
     }
-    //printf("the str is %s\n",p); //sholhim pointer she mazbia al klum pointer ezer she ishamesh ke maarah
+    
     print_r(root,p,0);
     //p meupas po
-    // display2(root,p,0,word);
-    //dofree(root);
+   
         
      if (argc == 1){
     while((k=getchar())!=EOF){
